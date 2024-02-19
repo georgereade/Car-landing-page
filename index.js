6bd1157905992159e334b3072f7083546a2861d4
georgereade <georgereade@hotmail.co.uk>
2024-02-19 16:33:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function slideIn() {
+  anime({
+    targets: "#main-car",
+    translateX: -300,
+    duration: 3000,
+    easing: "easeInOutExpo",
+    opacity: 1,
+  });
+}
+
+function fadeIn() {
+  anime({
+    targets: "#signup-container",
+    translateY: -50,
+    duration: 3000,
+    easing: "easeInOutExpo",
+    opacity: 1,
+    delay: 500,
+  });
+}
+
+// const scrollPercent = () => {
+//   const bodyST = document.body.scrollTop;
+//   const docST = document.documentElement.scrollTop;
+//   const docSH = document.documentElement.scrollHeight;
+//   const docCH = document.documentElement.clientHeight;
+
+//   return ((docST + bodyST) / (docSH - docCH)) * 100;
+// };
+
+// window.onscroll = () => {
+//   fadeIn.seek((scrollPercent() / 100) * fadeIn.duration);
+// };
+
+slideIn();
+fadeIn();
