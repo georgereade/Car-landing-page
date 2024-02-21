8ea0488ad7b917b7d1940dbd93c4594a7bb87a36
georgereade <georgereade@hotmail.co.uk>
2024-02-21 12:48:12 +0000

    Final commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-// function to
-function slideUp() {
+// animate the car entering the viewport
+function slideIn() {
   anime({
     targets: "#main-car",
     translateX: -300,
@@ -9,42 +9,45 @@ function slideUp() {
   });
 }
 
-slideUp();
+slideIn();
 
+// animate the main text becoming visible and rising
 function fadeIn() {
   anime({
     targets: ["#signup-container", "#top-header", "#features"],
     translateY: -30,
-    duration: 1000,
-    easing: "easeInOutExpo",
+    duration: 800,
+    easing: "easeInQuad",
     opacity: 1,
   });
 }
 
-function showFeatures() {
-  anime({
-    targets: "#features",
-    opacity: 1,
-    translateY: -25,
-    duration: 1000,
-    easing: "easeInOutSine",
-  });
-}
+// animate the features element becoming visible
+// function showFeatures() {
+//   anime({
+//     targets: "#features",
+//     opacity: 1,
+//     translateY: -25,
+//     duration: 1000,
+//     easing: "easeInOutSine",
+//   });
+// }
 
+// subtle animation of feature headers to draw attention
 function featuresPulse() {
   anime({
     targets: ".feature-header",
-    duration: 1000,
+    duration: 600,
     easing: "easeInOutSine",
     scale: 1.05,
-    loop: 10,
+    loop: 2,
     direction: "alternate",
   });
 }
 
 // add an event listener to fade in on scrolling
 window.addEventListener("scroll", scrollFunction);
-
+// call fadeIn and featuresPulse functions on scroll
 function scrollFunction() {
   if (window.scrollY > 5 || document.body.scrollTop > 5) {
     fadeIn();
@@ -115,9 +118,67 @@ function removeDiscover() {
 
 $(document).ready(function () {
   $("#discover-button").click(function () {
-    // showFeatures();
     hideDiscover();
     removeDiscover();
-    // document.getElementById("features").style.height = "100%";
   });
 });
+
+if ($(window).width() <= 1024) {
+  $("#comfort-image").click(function (e) {
+    e.preventDefault();
+    anime({
+      targets: "#comfort-text",
+      opacity: 1,
+      height: 160,
+      duration: 500,
+      easing: "easeInOutExpo",
+    });
+    anime({
+      targets: e.currentTarget,
+      opacity: 0.3,
+      duration: 700,
+      direction: "alternate",
+      easing: "easeInOutExpo",
+    });
+  });
+}
+
+if ($(window).width() <= 1024) {
+  $("#innovation-image").click(function (e) {
+    e.preventDefault();
+    anime({
+      targets: "#innovation-text",
+      opacity: 1,
+      height: 160,
+      duration: 500,
+      easing: "easeInOutExpo",
+    });
+    anime({
+      targets: e.currentTarget,
+      opacity: 0.3,
+      duration: 700,
+      direction: "alternate",
+      easing: "easeInOutExpo",
+    });
+  });
+}
+
+if ($(window).width() <= 1024) {
+  $("#safety-image").click(function (e) {
+    e.preventDefault();
+    anime({
+      targets: "#safety-text",
+      opacity: 1,
+      height: 160,
+      duration: 500,
+      easing: "easeInOutExpo",
+    });
+    anime({
+      targets: e.currentTarget,
+      opacity: 0.3,
+      duration: 700,
+      direction: "alternate",
+      easing: "easeInOutExpo",
+    });
+  });
+}
